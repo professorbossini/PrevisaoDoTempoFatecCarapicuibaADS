acffd69f7f62444388d02d137f7d04a3f280e6c5
rodrigo <professorbossini@gmail.com>
2018-05-22 10:27:25 -0300

    Continuar a partir do tratamento do JSON. Ver método onPostExecute.

--- app/src/main/java/br/com/bossini/previsaodotempofateccarapicuibaads/MainActivity.java
+++ app/src/main/java/br/com/bossini/previsaodotempofateccarapicuibaads/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.bossini.previsaodotempofateccarapicuibaads;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,10 +44,86 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 EditText locationEditText = findViewById(R.id.locationEditText);
                 String cidade = locationEditText.getEditableText().toString();
+                String url = montaURL(cidade);
+                ObtemTemperaturas o =
+                        new ObtemTemperaturas();
+                o.execute(url);
 
             }
         });
     }
 
+    private String montaURL (String cidade){
+        StringBuilder sb = new StringBuilder (
+                getString(R.string.web_service_url));
+                sb.append(cidade).
+                append("&APPID=").
+                append(getString(R.string.api_key)).
+                append("&units=metric&").
+                append("cnt=12");
+        return sb.toString();
+    }
 
+    class ObtemTemperaturas
+            extends AsyncTask <String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... url) {
+            try{
+
+                //instanciar a classe obter temperaturas
+                //executar
+                OkHttpClient client =
+                        new OkHttpClient();
+                Request request =
+                        new Request.Builder().
+                                url(url[0]).build();
+                Response response =
+                        client.newCall(request).execute();
+                String json = response.body().string();
+                return json;
+            }
+            catch (IOException e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try{
+                JSONObject jsonObj = new JSONObject(s);
+                JSONArray list = jsonObj.getJSONArray("list");
+                for (int i = 0; i < list.length(); i++){
+                    JSONObject dia = list.getJSONObject(i);
+                    long dt = dia.getLong("dt");
+                    String description =
+                            dia.getJSONArray("weather").
+                                    getJSONObject(0).
+                                    getString("description");
+                    String icon =
+                           dia.getJSONArray("weather").
+                           getJSONObject(0).
+                                   getString("icon");
+                    double min =
+                            dia.getJSONObject("temp").
+                                    getDouble("min");
+                    double max =
+                            dia.getJSONObject("temp").
+                                    getDouble("max");
+                    int humidity =
+                            dia.getInt("humidity");
+                    
+
+                }
+            }
+            catch (JSONException e){
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this,
+                        getString(R.string.read_error),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
 }
